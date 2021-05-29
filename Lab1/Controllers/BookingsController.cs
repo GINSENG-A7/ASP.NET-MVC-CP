405befa827d067081f15161f59a3cd858945adf8
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2021-05-29 21:59:38 +0300

    SQLRelationsToForeignTables

--- Lab1/Controllers/BookingsController.cs
+++ Lab1/Controllers/BookingsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -36,43 +37,12 @@ namespace Lab1.Controllers
             return View(booking);
         }
 
-        public ActionResult AvailableApartments(int? id, DateTime? settling, DateTime? eviction, int? vog, int? vok) 
-        {
-            ViewBag.ClientId = id;
-            string settlingStr = ConvertDateToString(settling);
-            string evictionStr = ConvertDateToString(eviction);
-            var requestResultTest = db.Database.SqlQuery<Apartments>($"SELECT * FROM Apartments b WHERE EXISTS (SELECT a.id FROM Apartments a WHERE b.id = a.id AND a.price > {0} AND a.price < {999999} AND (( EXISTS (SELECT apartmentsid FROM Livings WHERE eviction < '{settlingStr}' AND apartmentsid = a.id) AND NOT EXISTS(SELECT apartmentsid FROM Bookings WHERE EXISTS (SELECT apartmentsid FROM Bookings WHERE apartmentsid = a.id))) OR (EXISTS (SELECT apartmentsid FROM Bookings WHERE settling > '{evictionStr}' AND apartmentsid = a.id) OR ( EXISTS (SELECT apartmentsid FROM Bookings WHERE eviction < '{settlingStr}' AND apartmentsid = a.id))) AND NOT EXISTS(SELECT apartmentsid FROM Livings WHERE EXISTS (SELECT apartmentsid FROM Livings WHERE apartmentsid = a.id)) OR (( EXISTS (SELECT apartmentsid FROM Livings WHERE eviction<'{settlingStr}' AND apartmentsid = a.id)) AND ( EXISTS (SELECT apartmentsid FROM Bookings WHERE settling>'{evictionStr}' AND apartmentsid = a.id ) OR ( EXISTS (SELECT apartmentsid FROM Bookings WHERE eviction<'{settlingStr}' AND apartmentsid = a.id)))) OR ( NOT EXISTS (SELECT apartmentsid FROM Livings WHERE apartmentsid = a.id) AND NOT EXISTS (SELECT apartmentsid FROM Bookings WHERE apartmentsid = a.id))))");
-            return View("AvailableApartments", requestResultTest);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult AvailableApartments([Bind(Include = "Settling,Eviction,ValueOfGuests,ValueOfKids,ClientId")] Booking booking)
-        {
-            return View(booking);
-        }
         // GET: Bookings/Create
         //public ActionResult Create()
         //{
         //    return View();
         //}
-        public ActionResult BookingDateChooser(int? id)
-        {
-            ViewBag.ClientId = id;
-            return View("BookingDateChooser");
-        }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult BookingDateChooser([Bind(Include = "Settling,Eviction,ValueOfGuests,ValueOfKids,ClientId")] Booking booking)
-        {
-            if (ModelState.IsValid)
-            {
-                return RedirectToAction("AvailableApartments", new { id = booking.ClientId, settling = booking.Settling, eviction = booking.Eviction, vog = booking.ValueOfGuests, vok = booking.ValueOfKids });
-            }
-
-            return View(booking);
-        }
         public ActionResult Create(int? id)
         {
             ApartmentsDataLogistics();
@@ -192,15 +162,6 @@ namespace Lab1.Controllers
                 });
             }
             ViewBag.Client = new SelectList(viewClient, "ClientId", "ClientFIO");
-        }
-        public String ConvertDateToString(DateTime? dt)
-        {
-            string processingStr = dt.ToString();
-            processingStr = processingStr.Substring(0, 10);
-            string day = processingStr.Substring(0, 2);
-            string month = processingStr.Substring(3, 2);
-            string year = processingStr.Substring(6, 4);
-            return year + "-" + month + "-" + day;
         }
     }
 }
--- Lab1/Controllers/ClientsController.cs
+++ Lab1/Controllers/ClientsController.cs
@@ -70,7 +70,8 @@ namespace Lab1.Controllers
                 db.Clients.Add(client);
                 db.SaveChanges();
                 int cId = client.Id;
-                return RedirectToAction("BookingDateChooser", "Bookings", new { id = cId});
+                //return RedirectToAction("BookingDateChooser", "Bookings", new { id = cId});
+                return View("~/Views/Bookings/BookingDateChooser.cshtml");
                 //return RedirectToAction("Create", "Bookings", new { id = db.Clients.First(c => c.PassportSeries == client.PassportSeries && c.PassportNumber == client.PassportNumber).Id });
             }
 
